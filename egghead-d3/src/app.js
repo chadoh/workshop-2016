ae0358354c240783ea66fb419d7b313ada61d9ae
Chad Ostrowski <hi@chadoh.com>
2016-11-05 09:07:16 -0400

    Egghead D3v4 video 2

--- egghead-d3/src/app.js
+++ egghead-d3/src/app.js
@@ -1,1 +1,11 @@
-document.body.innerHTML = `<h1>Using D3v${d3.version}</h1>`
+var linearScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([0, 600])
+  .clamp(true)
+
+console.log(linearScale(-20))
+console.log(linearScale(0))
+console.log(linearScale(50))
+console.log(linearScale(105))
+
+console.log(linearScale.invert(900))
