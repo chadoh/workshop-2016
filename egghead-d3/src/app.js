448dd6de01e523c7071f319ae3ac4c5f3a912272
Chad Ostrowski <hi@chadoh.com>
2016-11-05 09:43:24 -0400

    Egghead D3v4 vid6: loading data

--- egghead-d3/src/app.js
+++ egghead-d3/src/app.js
@@ -1,7 +1,35 @@
-var ordinalScale = d3.scaleOrdinal()
-  .domain(["poor", "good", "great"])
-  .range(["red", "gray", "green"])
+// d3.csv('data/data.csv', function(data) {
+//   console.log(data)
+// })
 
-console.log(ordinalScale("poor"))
-console.log(ordinalScale("good"))
-console.log(ordinalScale("great"))
+// d3.tsv('data/data.tsv', function(data) {
+//   console.log(data)
+// })
+
+d3.json('data/data.json', function(data) {
+  var min = d3.min(data, function(d) {
+    return d.age
+  })
+  console.log(min)
+  var max = d3.max(data, function(d) {
+    return d.age
+  })
+  console.log(max)
+  var extent = d3.extent(data, function(d) {
+    return d.age
+  })
+  console.log(extent)
+
+  var scale = d3.scaleLinear()
+    .domain(extent)
+    .range([0, 600])
+  console.log(scale(14))
+  console.log(scale(22))
+  console.log(scale(37))
+
+  var ages = d3.set(data, function(d) {
+    return d.age;
+  })
+  console.log(ages.values())
+})
+
