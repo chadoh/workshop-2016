9905d801db99f8d0f2b4d24d234d1f135932e6a8
Chad Ostrowski <hi@chadoh.com>
2016-11-05 09:16:57 -0400

    Egghead D3v4 vid3

--- egghead-d3/src/app.js
+++ egghead-d3/src/app.js
@@ -1,11 +1,6 @@
-var linearScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([0, 600])
-  .clamp(true)
+var timeScale = d3.scaleTime()
+  .domain([new Date(2016, 0, 1), new Date()])
+  .range([0, 100])
 
-console.log(linearScale(-20))
-console.log(linearScale(0))
-console.log(linearScale(50))
-console.log(linearScale(105))
-
-console.log(linearScale.invert(900))
+console.log(timeScale.invert(50))
+console.log(timeScale(new Date()))
