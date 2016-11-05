26ec06eba15e495b49d22ca8aabb2c412b25fada
Chad Ostrowski <hi@chadoh.com>
2016-11-05 09:28:51 -0400

    Egghead D3v4 vid5: scaleOrdinal

--- egghead-d3/src/app.js
+++ egghead-d3/src/app.js
@@ -1,11 +1,7 @@
-var quantizeScale = d3.scaleQuantize()
-  .domain([0, 100])
+var ordinalScale = d3.scaleOrdinal()
+  .domain(["poor", "good", "great"])
   .range(["red", "gray", "green"])
 
-console.log(quantizeScale(1))
-console.log(quantizeScale(32))
-console.log(quantizeScale(34))
-console.log(quantizeScale(67))
-console.log(quantizeScale(100))
-
-console.log(quantizeScale.invertExtent("gray"))
+console.log(ordinalScale("poor"))
+console.log(ordinalScale("good"))
+console.log(ordinalScale("great"))
