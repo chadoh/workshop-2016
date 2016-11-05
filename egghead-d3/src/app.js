148dae94f6039ece84a56e9dfd68609c0e39c81b
Chad Ostrowski <hi@chadoh.com>
2016-11-05 17:43:02 -0400

    Egghead D3v4 vid9: DOM insertion

--- egghead-d3/src/app.js
+++ egghead-d3/src/app.js
@@ -1,3 +1,6 @@
-d3.select('a:nth-child(2)')
-  .classed('red', true)
-  .html('Inventory <b>Sale</b>')
+d3.select('.title')
+  .insert('div', 'a:nth-child(2)')
+    .html('Inventory <b>SALE</b>')
+
+d3.select('.action')
+  .remove()
