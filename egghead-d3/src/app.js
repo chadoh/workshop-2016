8ef9d855c71ec49acefd1d44a7081948782c9cfb
Chad Ostrowski <hi@chadoh.com>
2016-11-07 09:07:40 -0500

    Egghead D3v4 vid11: generating SVG

--- egghead-d3/src/app.js
+++ egghead-d3/src/app.js
@@ -7,21 +7,14 @@ var scores = [
 ]
 
 var update = d3.select('.chart')
-  .selectAll('div')
-  .data(scores, function(d) {
-    return d ? d.name : this.innerText;
-  })
-  .style('color', 'blue')
-
-var enter = update.enter()
-  .append('div')
-  .text(d => d.name)
-  .style('color', 'green')
-
-update.exit().remove()
-
-update.merge(enter)
-  .style('width', d => d.score + 'px')
-  .style('height', '50px')
-  .style('background', 'lightgreen')
-  .style('border', '1px solid black')
+  .append('svg')
+    .attr('width', 225)
+    .attr('height', 300)
+  .selectAll('rect')
+  .data(scores)
+  .enter()
+    .append('rect')
+    .attr('y', (d, i) => i * 33)
+    .text(d => `${d.name} (${d.score})`)
+    .style('width', d => d.score)
+    .classed('bar', true)
