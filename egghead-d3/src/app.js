e10fa71fda6fa740db7bf993792cf0c03e018639
Chad Ostrowski <hi@chadoh.com>
2016-11-07 09:18:21 -0500

    Egghead D3v4 vid12: SVG graphics containers & text

--- egghead-d3/src/app.js
+++ egghead-d3/src/app.js
@@ -6,15 +6,21 @@ var scores = [
   { name: "Emily", score: 88 },
 ]
 
-var update = d3.select('.chart')
+var bar = d3.select('.chart')
   .append('svg')
     .attr('width', 225)
     .attr('height', 300)
-  .selectAll('rect')
+  .selectAll('g')
   .data(scores)
   .enter()
-    .append('rect')
-    .attr('y', (d, i) => i * 33)
-    .text(d => `${d.name} (${d.score})`)
+    .append('g')
+    .attr('transform', (d, i) => `translate(0, ${i * 33})`)
+
+bar.append('rect')
     .style('width', d => d.score)
     .classed('bar', true)
+
+bar.append('text')
+    .attr('y', 20)
+    .attr('x', 5)
+    .text(d => `${d.name} (${d.score})`)
